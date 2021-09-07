9f137cb64c26d91050468ceb99dcbe42cbd79664
UnforgivenLalalend <acc4anim@gmail.com>
2021-09-07 23:32:07 +0300

    project initialisation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rand::Rng;
+
+fn generate_possible_prime(bits: u32) -> primitive_types::U512 {
+    let mut rng = rand::thread_rng();
+    let mut bytes = Vec::new();
+
+    for _ in 0..bits / 8 {
+        bytes.push(rng.gen::<u8>());
+    }
+    println!("{:?}\n\n{}", bytes, bytes.len());
+    primitive_types::U512::from_little_endian(&bytes)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", generate_possible_prime(512));
 }
